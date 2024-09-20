a3eff798934b225bb1ead96fc4102e928d06b409
AliAngelinaaa <nanzum1140@gmail.com>
2024-09-19 20:28:30 -0400

    made it modern

--- script.js
+++ script.js
@@ -1,265 +1,264 @@
+// Global variables
+let battleLog = "";
+let turns = 0;
+let moves = 0;
+let playerTurn = true;
+let cvar = 0;
+
+// Player stats
+let STRENGTH, CUNNING, SPEED, FATIGUE, oFATIGUE;
+
+// Computer stats
+let CSTRENGTH, CCUNNING, CSPEED, CFATIGUE, CoFATIGUE;
+
+// Game values
+let attackingd, defendingd, finishingd, cattackingd, cdefendingd, cfinishingd;
+
+// DOM elements
+const elements = {
+    attackingOutput: null,
+    defendingOutput: null,
+    finishingMoveOutput: null,
+    yourStrengthOutput: null,
+    yourSpeedOutput: null,
+    yourCunningOutput: null,
+    yourFatigueOutput: null,
+    youroriginalFatigueOutput: null,
+    cattackingOutput: null,
+    cdefendingOutput: null,
+    cfinishingMoveOutput: null,
+    computerStrengthOutput: null,
+    computerSpeedOutput: null,
+    computerFatigueOutput: null,
+    computerCunningOutput: null,
+    coriginalFatigueOutput: null,
+    battleLogOutput: null,
+    roundOutput: null,
+    finishingMoveBtn: null
+};
+
+const getRandomInteger = (lower, upper) => {
+    return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+};
+
+function initializeElements() {
+    elements.attackingOutput = document.getElementById("attackingdd");
+    elements.defendingOutput = document.getElementById("defendingdd");
+    elements.finishingMoveOutput = document.getElementById("finishingdd");
+    elements.yourStrengthOutput = document.getElementById("measuredSTRENGTH");
+    elements.yourSpeedOutput = document.getElementById("measuredSPEED");
+    elements.yourCunningOutput = document.getElementById("measuredCUNNING");
+    elements.yourFatigueOutput = document.getElementById("measuredFATIGUE");
+    elements.youroriginalFatigueOutput = document.getElementById("originalFATIGUE");
+    elements.cattackingOutput = document.getElementById("cattackingdd");
+    elements.cdefendingOutput = document.getElementById("cdefendingdd");
+    elements.cfinishingMoveOutput = document.getElementById("cfinishingdd");
+    elements.computerStrengthOutput = document.getElementById("measuredCSTRENGTH");
+    elements.computerSpeedOutput = document.getElementById("measuredCSPEED");
+    elements.computerFatigueOutput = document.getElementById("measuredCFATIGUE");
+    elements.computerCunningOutput = document.getElementById("measuredCCUNNING");
+    elements.coriginalFatigueOutput = document.getElementById("originalCFATIGUE");
+    elements.battleLogOutput = document.getElementById("battleLog");
+    elements.roundOutput = document.getElementById("turns");
+    elements.finishingMoveBtn = document.getElementById("FMBtn");
+}
+
 function initialize() {
-
-    //player info
-
-    attackingOutput = document.getElementById("attackingdd");
-    defendingOutput = document.getElementById("defendingdd");
-    finishingMoveOutput = document.getElementById("finishingdd");
-
-    yourStrengthOutput = document.getElementById("measuredSTRENGTH");
-    yourSpeedOutput = document.getElementById("measuredSPEED");
-    yourCunningOutput = document.getElementById("measuredCUNNING");
-    yourFatigueOutput = document.getElementById("measuredFATIGUE");
-    youroriginalFatigueOutput = document.getElementById("originalFATIGUE");
-
-    //computer info
-
-    cattackingOutput = document.getElementById("cattackingdd");
-    cdefendingOutput = document.getElementById("cdefendingdd");
-    cfinishingMoveOutput = document.getElementById("cfinishingdd");
-
-    computerStrengthOutput = document.getElementById("measuredCSTRENGTH");
-    computerSpeedOutput = document.getElementById("measuredCSPEED");
-    computerFatigueOutput = document.getElementById("measuredCFATIGUE");
-    computerCunningOutput = document.getElementById("measuredCCUNNING");
-    coriginalFatigueOutput = document.getElementById("originalCFATIGUE");
-
-    //keep track info
-
-    battleLogOutput = document.getElementById("battleLog");
-    roundOutput = document.getElementById("turns");
+    // Reset game state
     battleLog = "";
     turns = 0;
     moves = 0;
+    playerTurn = true;
+    cvar = 0;
 
-    //the player's status generate
-
+    // Generate player stats
     STRENGTH = 6 + getRandomInteger(-1, 1);
     CUNNING = 6 + getRandomInteger(-1, 1);
     SPEED = 6 + getRandomInteger(-1, 1);
     FATIGUE = 30 + getRandomInteger(-6, 6);
     oFATIGUE = FATIGUE;
 
-    //the computer's status generate
-
+    // Generate computer stats
     CSTRENGTH = 6 + getRandomInteger(-1, 1);
     CCUNNING = 6 + getRandomInteger(-1, 1);
     CSPEED = 6 + getRandomInteger(-1, 1);
     CFATIGUE = 30 + getRandomInteger(-6, 6);
     CoFATIGUE = CFATIGUE;
-    //
-    cvar = 0;
-    document.getElementById("FMBtn").disabled = true;
 
-    // these will be zeros for now
+    // Initialize game values
+    if (elements.finishingMoveBtn) {
+        elements.finishingMoveBtn.disabled = true;
+    }
     attackingd = 0;
     defendingd = 0;
     finishingd = STRENGTH + SPEED;
-
     cattackingd = 0;
     cdefendingd = 0;
     cfinishingd = CSPEED + CSTRENGTH;
 
-    playerTurn = true;
-
     display();
 }
-// computer portion
+
 function makeComputerMove() {
-    if (playerTurn == false) {
+    if (!playerTurn) {
         cvar = getRandomInteger(1, 3);
 
-        if (cvar == 3) {
-            if (CFATIGUE > FATIGUE * 2) {
-                gameOver();
-            }
-            else {
-                if (FATIGUE < 0) {
-                    gameOver();
-                }
-                else {
-                    cvar = getRandomInteger(1, 2);
-                }
-            }
+        if (cvar === 3 && (CFATIGUE > FATIGUE * 2 || FATIGUE < 0)) {
+            gameOver();
+            return;
         }
 
-        if (cvar == 1) {
+        if (cvar === 1) {
             cattackingd = (CSTRENGTH + CCUNNING + CSPEED) / getRandomInteger(1, 3);
 
-            if (moves == 2) {
-                if (cattackingd > defendingd) {
-                    FATIGUE = FATIGUE - (cattackingd - (SPEED + getRandomInteger(1, 6)));
-                    if (FATIGUE > oFATIGUE) {
-                        FATIGUE = oFATIGUE;
-                    }
-
-                    battleLog = "The computer attacked you and now your fatigue is now " + FATIGUE + ".<br/>" + battleLog;
-                }
+            if (moves === 2 && cattackingd > defendingd) {
+                FATIGUE -= (cattackingd - (SPEED + getRandomInteger(1, 6)));
+                FATIGUE = Math.min(FATIGUE, oFATIGUE);
+                battleLog = `The computer attacked you and now your fatigue is ${FATIGUE}.<br/>${battleLog}`;
+            } else {
+                battleLog = `The computer attacked you but you defended yourself successfully so nothing changed.<br/>${battleLog}`;
             }
-            else {
-                battleLog = "The computer attacked you but you defended yourself successfully so nothing changed. <br/>" + battleLog;
-            }
+        } else if (cvar === 2) {
+            cdefendingd = CCUNNING + CSPEED;
+            battleLog = `The computer is defending itself.<br/>${battleLog}`;
+        } else {
+            cdefendingd = CSPEED + getRandomInteger(1, 6);
         }
-        else {
-            if (cvar == 2) {
-                cdefendingd = (CCUNNING + CSPEED);
 
-                battleLog = "The computer is defending itself. <br/>" + battleLog;
-            }
-            else {
-                cdefendingd = (CSPEED + getRandomInteger(1, 6));
-
-                if (cvar == 3) {
-                    if (CFATIGUE > FATIGUE * 2) {
-                        gameOver();
-                    }
-                    else {
-                        cvar = getRandomInteger(1, 2);
-                    }
-                }
-                if (cvar == 3) {
-                    if (FATIGUE < 0) {
-                        gameOver();
-                    }
-                    else {
-                        cvar = getRandomInteger(1, 2);
-                    }
-
-                }
-            }
+        if (CoFATIGUE !== CFATIGUE && (CoFATIGUE - CFATIGUE) % 5 === 0) {
+            CSTRENGTH--;
+            CSPEED--;
+            CCUNNING--;
         }
-        if (!(CoFATIGUE == CFATIGUE)) {
-            if ((CoFATIGUE - CFATIGUE) % 5 == 0) {
-                CSTRENGTH = CSTRENGTH - 1;
-                CSPEED = CSPEED - 1;
-                CCUNNING = CCUNNING - 1;
-            }
-        }
-        turns += 1;
+
+        turns++;
         playerTurn = true;
     }
     display();
     console.log(cvar);
 }
-function getRandomInteger(lower, upper) {
-    var multiplier = upper - (lower - 1);
-    var rnd = parseInt(Math.random() * multiplier) + lower;
 
-    return rnd;
-}
-//player portion
 function makeMove() {
-    if (playerTurn == true) {
+    if (playerTurn) {
         FinishingBtn();
 
-        if (moves == 1) {
+        if (moves === 1) {
             attackingd = (STRENGTH + CUNNING + SPEED) / getRandomInteger(1, 3);
 
-            if (cvar == 2) {
+            if (cvar === 2) {
                 if (attackingd > cdefendingd) {
-                    CFATIGUE = CFATIGUE - (attackingd - (SPEED + getRandomInteger(1, 6)));
-                    if (CFATIGUE > CoFATIGUE) {
-                        CFATIGUE = CoFATIGUE;
-                    }
-
-                    battleLog = "You attacked your opponent and now their fatigue is now " + CFATIGUE + ".<br/>" + battleLog;
+                    CFATIGUE -= (attackingd - (SPEED + getRandomInteger(1, 6)));
+                    CFATIGUE = Math.min(CFATIGUE, CoFATIGUE);
+                    battleLog = `You attacked your opponent and now their fatigue is ${CFATIGUE}.<br/>${battleLog}`;
+                } else {
+                    battleLog = `You attacked your opponent but they defended themselves successfully so nothing changed.<br/>${battleLog}`;
                 }
-                else {
-                    battleLog = "You attacked your opponent but they defended themselves successfully so nothing changed. <br/>" + battleLog;
-                }
+            } else {
+                battleLog = `You attacked your opponent.<br/>${battleLog}`;
             }
-            else {
-                battleLog = "You attacked your opponent. <br/>" + battleLog;
-            }
-
-        }
-        else {
-            if (moves == 2) {
-                defendingd = (CUNNING + SPEED);
-
-                battleLog = "You are now defending. <br/>" + battleLog;
-            }
-            else {
-                defendingd = (SPEED + getRandomInteger(1, 6));
-
-                if (moves == 3) {
-                    if (FATIGUE > CFATIGUE * 2) {
-                        gameOver();
-                    }
-                    else {
-                        if (CFATIGUE < 0) {
-                            gameOver();
-                        }
-                        else {
-                            battleLog = "You can't do anything yet so you just lost a turn. <br/>" + battleLog;
-                        }
-                    }
-                }
+        } else if (moves === 2) {
+            defendingd = CUNNING + SPEED;
+            battleLog = `You are now defending.<br/>${battleLog}`;
+        } else if (moves === 3) {
+            defendingd = SPEED + getRandomInteger(1, 6);
+            if (FATIGUE > CFATIGUE * 2 || CFATIGUE < 0) {
+                gameOver();
+            } else {
+                battleLog = `You can't do anything yet so you just lost a turn.<br/>${battleLog}`;
             }
         }
-        if (!(oFATIGUE == FATIGUE)) {
-            if ((oFATIGUE - FATIGUE) % 5 == 0) {
-                STRENGTH = STRENGTH - 1;
-                SPEED = SPEED - 1;
-                CUNNING = CUNNING - 1;
-            }
+
+        if (oFATIGUE !== FATIGUE && (oFATIGUE - FATIGUE) % 5 === 0) {
+            STRENGTH--;
+            SPEED--;
+            CUNNING--;
         }
-        turns += 1;
+
+        turns++;
         playerTurn = false;
     }
     display();
     makeComputerMove();
 }
 
-//button function
-
 function FinishingBtn() {
-    if (FATIGUE > CFATIGUE * 2) {
-        document.getElementById("FMBtn").disabled = false;
+    const canFinish = FATIGUE > CFATIGUE * 2 || CFATIGUE < 0;
+    if (elements.finishingMoveBtn) {
+        elements.finishingMoveBtn.disabled = !canFinish;
     }
-    else {
-        if (CFATIGUE < 0) {
-            document.getElementById("FMBtn").disabled = false;
-        }
-        else {
-            battleLog = "You cannot perform finishing move <br/>" + battleLog;
-            document.getElementById("FMBtn").disabled = true;
-        }
+    if (!canFinish) {
+        battleLog = `You cannot perform finishing move<br/>${battleLog}`;
     }
 }
-//ending scenes and display
+
 function gameOver() {
-    if (cvar == 3) {
-        battleLog = "You lost. The computer won. <br/>" + battleLog;
-        initialize();
+    if (cvar === 3 || FATIGUE <= 0) {
+        battleLog = `You lost. The computer won.<br/>${battleLog}`;
+    } else if (moves === 3 || CFATIGUE <= 0) {
+        battleLog = `You won!<br/>${battleLog}`;
     }
-    if (moves == 3) {
-        battleLog = "You won! <br/>" + battleLog;
-        initialize();
-    }
+    initialize();
 }
+
 function display() {
-    attackingOutput.innerHTML = attackingd;
-    defendingOutput.innerHTML = defendingd;
-    finishingMoveOutput.innerHTML = finishingd;
-
-    cattackingOutput.innerHTML = cattackingd;
-    cdefendingOutput.innerHTML = cdefendingd;
-    cfinishingMoveOutput.innerHTML = cfinishingd;
-
-    roundOutput.innerHTML = turns;
-
-    yourStrengthOutput.innerHTML = STRENGTH;
-    yourSpeedOutput.innerHTML = SPEED;
-    yourCunningOutput.innerHTML = CUNNING;
-    yourFatigueOutput.innerHTML = FATIGUE;
-    youroriginalFatigueOutput.innerHTML = oFATIGUE;
-
-    computerStrengthOutput.innerHTML = CSTRENGTH;
-    computerSpeedOutput.innerHTML = CSPEED;
-    computerCunningOutput.innerHTML = CCUNNING;
-    computerFatigueOutput.innerHTML = CFATIGUE;
-    coriginalFatigueOutput.innerHTML = CoFATIGUE;
-
-    battleLogOutput.innerHTML = battleLog;
+    if (elements.attackingOutput) {
+        elements.attackingOutput.innerHTML = attackingd;
+    }
+    if (elements.defendingOutput) {
+        elements.defendingOutput.innerHTML = defendingd;
+    }
+    if (elements.finishingMoveOutput) {
+        elements.finishingMoveOutput.innerHTML = finishingd;
+    }
+    if (elements.cattackingOutput) {
+        elements.cattackingOutput.innerHTML = cattackingd;
+    }
+    if (elements.cdefendingOutput) {
+        elements.cdefendingOutput.innerHTML = cdefendingd;
+    }
+    if (elements.cfinishingMoveOutput) {
+        elements.cfinishingMoveOutput.innerHTML = cfinishingd;
+    }
+    if (elements.roundOutput) {
+        elements.roundOutput.innerHTML = turns;
+    }
+    if (elements.yourStrengthOutput) {
+        elements.yourStrengthOutput.innerHTML = STRENGTH;
+    }
+    if (elements.yourSpeedOutput) {
+        elements.yourSpeedOutput.innerHTML = SPEED;
+    }
+    if (elements.yourCunningOutput) {
+        elements.yourCunningOutput.innerHTML = CUNNING;
+    }
+    if (elements.yourFatigueOutput) {
+        elements.yourFatigueOutput.innerHTML = FATIGUE;
+    }
+    if (elements.youroriginalFatigueOutput) {
+        elements.youroriginalFatigueOutput.innerHTML = oFATIGUE;
+    }
+    if (elements.computerStrengthOutput) {
+        elements.computerStrengthOutput.innerHTML = CSTRENGTH;
+    }
+    if (elements.computerSpeedOutput) {
+        elements.computerSpeedOutput.innerHTML = CSPEED;
+    }
+    if (elements.computerCunningOutput) {
+        elements.computerCunningOutput.innerHTML = CCUNNING;
+    }
+    if (elements.computerFatigueOutput) {
+        elements.computerFatigueOutput.innerHTML = CFATIGUE;
+    }
+    if (elements.coriginalFatigueOutput) {
+        elements.coriginalFatigueOutput.innerHTML = CoFATIGUE;
+    }
+    if (elements.battleLogOutput) {
+        elements.battleLogOutput.innerHTML = battleLog;
+    }
 }
+
+// Initialize the game when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    initializeElements();
+    initialize();
+});
